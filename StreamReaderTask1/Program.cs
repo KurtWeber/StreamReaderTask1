241648241c65ad2e4d689daf955e41ede4fd2bf6
Kurt Weber <weberkrt@msn.com>
2016-09-21 20:18:14 -0400

    StreamReader Task1 complete

---
+++ StreamReaderTask1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;    
+
+namespace StreamReaderTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of StreamReader to read from a file
+            StreamReader reader = new StreamReader("..\\..\\..\\Shakespeare.txt");
+
+            // Read first line from the text file
+            string line = reader.ReadLine();
+
+            Console.WriteLine(line);
+
+            // Close the resource after you've finished using it
+            reader.Close();            
+        }
+    }
+}
